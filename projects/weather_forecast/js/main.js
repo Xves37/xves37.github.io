f30bca3131e80e41194d38ee3e67cf065ea6f744
Daniil Khvesiuk <danik37h@gmail.com>
2018-11-08 10:28:22 +0300

    min-width

--- projects/weather_forecast/js/main.js
+++ projects/weather_forecast/js/main.js
@@ -1,6 +1,14 @@
-let btn = document.querySelector('button.button');
+let btn       = document.querySelector('button.button');
 let cityInput = document.querySelector('.city-input');
-let response;
+let error     = document.querySelector('.error');
+let loader    = document.querySelector('.round');
+let conteiner = document.querySelector('.info');
+
+let name     = document.querySelector('.city-name');
+let weather  = document.querySelector('.weather');
+let temp     = document.querySelector('.temp');
+let press    = document.querySelector('.press');
+let humudity = document.querySelector('.humidity');
 
 btn.addEventListener('click', () => ajaxGet(cityInput.value))
 
@@ -8,17 +16,47 @@ function ajaxGet(city) {
 
     let request = new XMLHttpRequest();
 
+    loader.classList.add('round-animation');
+
     request.onreadystatechange = function () {
         if (request.readyState == 4 && request.status == 200) {
-            response = JSON.parse(request.responseText);
-            console.log(response)
+
+            conteiner.classList.add('info-visible');
+            let response = JSON.parse(request.responseText);
+            console.log(response);
+            changeInfo(response);
+            wrongInputBack();
+            loader.classList.remove('round-animation');
+
         } else if (request.readyState == 4 && request.status == 404) {
-            console.log('You entered wrong city')
+            wrongInput('You entered wrong city!')
+            loader.classList.remove('round-animation');
+        } else if (request.readyState == 4) {
+            wrongInput('Enter something!')
+            loader.classList.remove('round-animation');
         }
     }
 
-    request.open('GET', '//api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=ef5ffdb295f9241df26ba3b904510af5');
+    request.open('GET', 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=ef5ffdb295f9241df26ba3b904510af5');
     request.send();
 
 }
 
+function changeInfo(response) {
+    name.innerHTML     = response.name;
+    weather.innerHTML  = response.weather[0].main;
+    temp.innerHTML     = Math.round(response.main.temp - 273.15);
+    press.innerHTML    = response.main.pressure;
+    humudity.innerHTML = response.main.humidity;
+}
+
+function wrongInput(errorText) {
+    error.innerHTML = errorText;
+    error.classList.add('error-visible');
+    cityInput.classList.add('input-error');
+}
+
+function wrongInputBack() {
+    error.classList.remove('error-visible');
+    cityInput.classList.remove('input-error');
+}
